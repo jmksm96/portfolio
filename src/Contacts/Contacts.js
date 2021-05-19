ba2002933d729f845b30bb4f015d6687986d5006
Max <jmksm96@gmail.com>
2021-05-19 17:54:14 +0300

    3.1 Contacts

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import style from './Contacts.module.scss'
-import styleContainer from '../Common/Styles/Container.module.css'
 import Title from "../Common/Components/Title/Title";
 
 
 const Contacts = () => {
     return (
         <div className={style.mainBlock}>
-            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+            <div className={style.skillsContainer}>
                 <Title text={'Contacts'}/>
                 <form className={style.formContainer}>
                         <input type="text" className={style.formArea} placeholder={'Name'}/>
                         <input type="text" className={style.formArea} placeholder={'E-mail'}/>
-                        <textarea placeholder={'Message'}/>
+                        <textarea placeholder={'Message'} className={style.messageArea}/>
                         <button type="submit" className={style.button}>Send message</button>
                 </form>
             </div>
