5f2743fd639dc776592c562fc0fc3df0c386c4f8
Max <jmksm96@gmail.com>
2021-05-19 18:28:54 +0300

    3.2 Footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import style from './Footer.module.scss'
-import styleContainer from '../Common/Styles/Container.module.css'
 import Title from "../Common/Components/Title/Title";
 
 const Footer = () => {
     return (
         <div className={style.mainBlock}>
-            <div className={`${styleContainer.container} ${style.footerContainer}`}>
+            <div className={style.footerContainer}>
                 <Title text = {'Maxim Yarosh'}/>
-                <div className={style.block}>
-                    <div className={style.blockContainer}>
-                        <div className={style.blockItem}></div>
-                        <div className={style.blockItem}></div>
-                        <div className={style.blockItem}></div>
-                        <div className={style.blockItem}></div>
+                <div className={style.socialIcons}>
+                    <div className={style.socialIcon}>
+                        <a href="">
+                            {/*<img src={} alt=""/>*/}
+                        </a>
                     </div>
-
+                    <div className={style.socialIcon}>
+                        <a href="">
+                            {/*<img src={} alt=""/>*/}
+                        </a>
+                    </div>
+                    <div className={style.socialIcon}>
+                        <a href="">
+                            {/*<img src={} alt=""/>*/}
+                        </a>
+                    </div>
                 </div>
-                <div className={style.footerDescription}>©2021 Все права защищены</div>
+                <div className={style.copyright}>©2021 Все права защищены</div>
             </div>
         </div >
     )
